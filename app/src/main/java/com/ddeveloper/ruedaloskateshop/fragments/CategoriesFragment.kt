63ebe79e322412ecdcd130c321f5c0e6c76d4c64
DAvid <david.vt.sb@gmail.com>
2025-04-28 18:47:41 -0500

    update

--- app/src/main/java/com/ddeveloper/ruedaloskateshop/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/ddeveloper/ruedaloskateshop/fragments/CategoriesFragment.kt
@@ -1,4 +1,13 @@
 package com.ddeveloper.ruedaloskateshop.fragments
 
-class CategoriesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ddeveloper.ruedaloskateshop.R
+
+class CategoriesFragment : Fragment() {
+
+
 }
